95032f067e3b25a7ce6695faca5e0601d87c1587
annatolchynska <anna.t1417@gmail.com>
2023-03-04 12:39:11 +0000

    Prepare for deployment.

--- p5_drf_api/urls.py
+++ p5_drf_api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from .views import root_route
+from .views import root_route, logout_route
 from django.urls import path, include
 
 urlpatterns = [
